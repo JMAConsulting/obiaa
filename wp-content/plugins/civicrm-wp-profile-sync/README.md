30ef387e243230ef6a7ad49bff5c48eff1431ecd
www-data <www-data@ovh3.jmaconsulting.biz>
2022-08-10 03:45:54 -0400

    Properly add in civicrm wp profile sync

---
+++ wp-content/plugins/civicrm-wp-profile-sync/README.md
@@ -0,0 +1,98 @@
+CiviCRM Profile Sync
+====================
+
+**Contributors:** [needle](https://profiles.wordpress.org/needle/), [cuny-academic-commons](https://profiles.wordpress.org/cuny-academic-commons/), [kcristiano](https://profiles.wordpress.org/kcristiano/), [tadpolecc](https://profiles.wordpress.org/tadpolecc/)<br/>
+**Donate link:** https://www.paypal.me/interactivist<br/>
+**Tags:** civicrm, user, buddypress, acf, profile, xprofile, sync<br/>
+**Requires at least:** 4.9<br/>
+**Tested up to:** 5.9<br/>
+**Stable tag:** 0.5.8a<br/>
+**License:** GPLv2 or later<br/>
+**License URI:** https://www.gnu.org/licenses/gpl-2.0.html
+
+Keeps a WordPress User profile in sync with a CiviCRM Contact and integrates WordPress and CiviCRM Entities with data synced via Advanced Custom Fields.
+
+
+
+## Description
+
+Please note: this is the development repository for CiviCRM Profile Sync. The plugin can be found in the [WordPress Plugin Directory](https://wordpress.org/plugins/civicrm-wp-profile-sync/), which is the best place to get it from if you're not a developer.
+
+### WordPress Integration
+
+At its simplest, the CiviCRM Profile Sync plugin keeps the "First Name", "Last Name", "Nickname", "Email Address" and "Website" fields of a WordPress User Profile in sync with their corresponding fields in a CiviCRM Contact. The synchronisation takes place regardless of whether the changes are made in WordPress or CiviCRM.
+
+### BuddyPress Integration
+
+The plugin also supports syncing the "First Name" and "Last Name" fields of the WordPress User and CiviCRM Contact with BuddyPress when using the BP xProfile WordPress User Sync plugin. Further integration with BuddyPress is in the pipeline.
+
+### ACF Integration
+
+CiviCRM Profile Sync enables integration between CiviCRM Entities and WordPress Entities with data synced via Advanced Custom Fields.
+
+Whilst ACF integration is not complete in its coverage of the CiviCRM Entities that can be linked, it is fairly comprehensive in its mapping of the built-in CiviCRM Custom Field Types with their corresponding ACF Field Types.
+
+So if, for example, you want to display (or create) a Contact Type on your WordPress site with ACF Fields that contain synced CiviCRM data, this feature could work for you.
+
+Please refer to the [ACF Integration Documentation](/docs/ACF.md) for details.
+
+### Form-building with ACF Extended
+
+CiviCRM Profile Sync enables Forms to be built for the front-end of your website with the UI provided by the ACF Extended plugin. These Forms can send their data directly to CiviCRM in a similar (though more limited) way to Caldera Forms CiviCRM.
+
+Form building with ACF Extended is at an early stage of development and is currently limited to submitting data for Contacts, Participants, Activities and Cases. This does, however, provide enough functionality to build some fairly powerful and useful Forms.
+
+Please refer to the [ACFE Form-building Documentation](/docs/ACFE.md) for details.
+
+### Requirements
+
+This plugin recommends a minimum of WordPress 4.9 and CiviCRM 5.23.
+
+If you want to maintain sync with BuddyPress user profiles, then it also requires BuddyPress 3.0 and the [BP xProfile WordPress User Sync](https://wordpress.org/plugins/bp-xprofile-wp-user-sync/) plugin to do so.
+
+For integration with Advanced Custom Fields, this plugin recommends a minimum of ACF 5.8 or ACF Pro 5.8.
+
+
+
+## Installation
+
+There are two ways to install from GitHub:
+
+### ZIP Download
+
+If you have downloaded CiviCRM Profile Sync as a ZIP file from the GitHub repository, do the following to install the plugin:
+
+1. Unzip the .zip file and, if needed, rename the enclosing folder so that the plugin's files are located directly inside `/wp-content/plugins/civicrm-wp-profile-sync`
+2. CiviCRM Profile Sync is installed. Read on for activation
+
+### git clone
+
+If you have cloned the code from GitHub, it is assumed that you know what you're doing.
+
+
+
+## Activation
+
+CiviCRM Profile Sync has some particular requirements when it comes to how it is activated. These correspond to the different types of WordPress install:
+
+### Single Site
+
+Easy - just activate the plugin and you are done!
+
+### Multisite
+
+Since Users and User metadata are stored in a single place for all sites, CiviCRM Profile Sync's User Profile settings cannot be configured on a per-site basis. This means that (whether CiviCRM Profile Sync is network-activated or not) whichever settings page you go to, you will see CiviCRM Profile Sync settings that are held in common for all sites.
+
+It is, of course, possible to activate CiviCRM in many different ways in Multisite - it could be network-activated, activated on the main site, and/or activated on one or more sub-sites. CiviCRM could also be in Multi-Domain mode or each instance could have its own database. It is recommended that you activate CiviCRM Profile Sync in the same way that CiviCRM is activated.
+
+If CiviCRM is not in Multi-Domain mode, you may have to write some custom code to propagate changed User details to other CiviCRM instances because (depending on the site on which a particular User's details are changed) only the Contact on the CiviCRM instance linked to that site will be updated.
+
+Test early, test often and - above all - test on a development site first.
+
+### Multi-Network
+
+In Multi-Network, Users and User metadata are stored in one place for all Networks but `site_options` are stored on a per-Network basis. As a result, it is not simple for CiviCRM Profile Sync to store a single collection of User Profile settings for all Networks. You will have to make sure that they are the same across all the Networks where both CiviCRM and this plugin are activated.
+
+A consequence of this architecture is that (depending on how you have set up CiviCRM across the Networks) there may be mismatches between the User data in WordPress and the Contact data in your CiviCRM instances. If CiviCRM is not in Multi-Domain mode, you will have to write some custom code to keep the data synced between WordPress and your CiviCRM instances.
+
+I'll say it again: test early, test often and - above all - test on a development site first.
---
+++ wp-content/plugins/civicrm-wp-profile-sync/includes/civicrm/cwps-civicrm-email.php
@@ -0,0 +1,626 @@
+<?php
+/**
+ * CiviCRM Email compatibility Class.
+ *
+ * Handles CiviCRM Email integration.
+ *
+ * @package CiviCRM_WP_Profile_Sync
+ * @since 0.4
+ */
+
+// Exit if accessed directly.
+defined( 'ABSPATH' ) || exit;
+
+/**
+ * CiviCRM Profile Sync CiviCRM Email compatibility Class.
+ *
+ * This class provides CiviCRM Email integration.
+ *
+ * @since 0.4
+ */
+class CiviCRM_WP_Profile_Sync_CiviCRM_Email {
+
+	/**
+	 * Plugin object.
+	 *
+	 * @since 0.4
+	 * @access public
+	 * @var object $plugin The plugin object.
+	 */
+	public $plugin;
+
+	/**
+	 * Parent (calling) object.
+	 *
+	 * @since 0.4
+	 * @access public
+	 * @var object $civicrm The parent object.
+	 */
+	public $civicrm;
+
+	/**
+	 * Mapper hooks registered flag.
+	 *
+	 * @since 0.5.2
+	 * @access public
+	 * @var bool $mapper_hooks The Mapper hooks registered flag.
+	 */
+	public $mapper_hooks = false;
+
+	/**
+	 * Initialises this object.
+	 *
+	 * @since 0.4
+	 *
+	 * @param object $parent The parent object.
+	 */
+	public function __construct( $parent ) {
+
+		// Store plugin reference.
+		$this->plugin = $parent->plugin;
+
+		// Store CiviCRM object reference.
+		$this->civicrm = $parent;
+
+		// Init when the CiviCRM object is loaded.
+		add_action( 'cwps/civicrm/loaded', [ $this, 'initialise' ] );
+
+	}
+
+	/**
+	 * Do stuff on plugin init.
+	 *
+	 * @since 0.4
+	 */
+	public function initialise() {
+
+		// Always register plugin hooks.
+		add_action( 'cwps/plugin/hooks/civicrm/add', [ $this, 'register_mapper_hooks' ] );
+		add_action( 'cwps/plugin/hooks/civicrm/remove', [ $this, 'unregister_mapper_hooks' ] );
+
+		// Register hooks.
+		$this->register_hooks();
+
+	}
+
+	/**
+	 * Register hooks.
+	 *
+	 * @since 0.4
+	 */
+	public function register_hooks() {
+
+		// Always register Mapper callbacks.
+		$this->register_mapper_hooks();
+
+		// Prevent CiviCRM from syncing Primary Email with WordPress User.
+		add_action( 'civicrm_postSave_civicrm_setting', [ $this, 'sync_setting_override' ], 10 );
+
+		// Show a notice in CiviCRM's WordPress Settings.
+		add_action( 'civicrm/metabox/email_sync/pre', [ $this, 'sync_setting_notice' ], 10 );
+		add_action( 'civicrm/metabox/email_sync/post', [ $this, 'sync_setting_js' ], 10 );
+		add_filter( 'civicrm/metabox/email_sync/submit/options', [ $this, 'sync_setting_button' ], 10 );
+
+		// Listen for User sync.
+		add_action( 'cwps/wordpress/user_sync', [ $this, 'primary_update' ], 10 );
+
+	}
+
+	/**
+	 * Unregister hooks.
+	 *
+	 * @since 0.4
+	 */
+	public function unregister_hooks() {
+
+		// Unregister Mapper callbacks.
+		$this->unregister_mapper_hooks();
+
+		// Remove all other callbacks.
+		remove_action( 'civicrm_postSave_civicrm_setting', [ $this, 'sync_setting_override' ], 10 );
+		remove_action( 'cwps/wordpress/user_sync', [ $this, 'email_update' ], 10 );
+
+	}
+
+	/**
+	 * Register Mapper hooks.
+	 *
+	 * @since 0.4
+	 */
+	public function register_mapper_hooks() {
+
+		// Bail if already registered.
+		if ( $this->mapper_hooks === true ) {
+			return;
+		}
+
+		// Intercept Email updates in CiviCRM.
+		add_action( 'cwps/mapper/email/edit/pre', [ $this, 'primary_pre' ], 10 );
+		add_action( 'cwps/mapper/email/edited', [ $this, 'primary_edited' ], 10 );
+
+		// Declare registered.
+		$this->mapper_hooks = true;
+
+	}
+
+	/**
+	 * Unregister Mapper hooks.
+	 *
+	 * @since 0.4
+	 */
+	public function unregister_mapper_hooks() {
+
+		// Bail if already unregistered.
+		if ( $this->mapper_hooks === false ) {
+			return;
+		}
+
+		// Remove all CiviCRM callbacks.
+		remove_action( 'cwps/mapper/email/edit/pre', [ $this, 'primary_pre' ], 10 );
+		remove_action( 'cwps/mapper/email/edited', [ $this, 'primary_edited' ], 10 );
+
+		// Declare unregistered.
+		$this->mapper_hooks = false;
+
+	}
+
+	// -------------------------------------------------------------------------
+
+	/**
+	 * Get the current value of the CiviCRM "Sync CMS Email" setting.
+	 *
+	 * @since 0.4
+	 *
+	 * @return bool $value Numeric 1 or 0 depending on the CiviCRM setting, or false otherwise.
+	 */
+	public function sync_setting_get() {
+
+		// Init return.
+		$value = false;
+
+		// Try and init CiviCRM.
+		if ( ! $this->civicrm->is_initialised() ) {
+			return $value;
+		}
+
+		// Get setting value.
+		$params = [
+			'version' => 3,
+			'name' => 'syncCMSEmail',
+			'group' => 'CiviCRM Preferences',
+		];
+
+		// Return the setting.
+		$result = civicrm_api( 'Setting', 'getvalue', $params );
+
+		// Bail if there's an error.
+		if ( ! empty( $result['is_error'] ) && $result['is_error'] == 1 ) {
+			return $value;
+		}
+
+		// Overwrite value with result.
+		$value = $result ? 1 : 0;
+
+		// Return the setting.
+		return $value;
+
+	}
+
+	/**
+	 * Set the CiviCRM "Sync CMS Email" setting.
+	 *
+	 * @since 0.4
+	 *
+	 * @param bool $value The value to apply to the setting.
+	 */
+	public function sync_setting_set( $value = false ) {
+
+		// Try and init CiviCRM.
+		if ( ! $this->civicrm->is_initialised() ) {
+			return;
+		}
+
+		// Switch setting to false.
+		$params = [
+			'version' => 3,
+			'syncCMSEmail' => $value,
+		];
+
+		// Save the setting.
+		$result = civicrm_api( 'Setting', 'create', $params );
+
+	}
+
+	/**
+	 * Set the CiviCRM "Sync CMS Email" setting without listening.
+	 *
+	 * @since 0.4
+	 *
+	 * @param bool $value The value to apply to the setting.
+	 */
+	public function sync_setting_force( $value = false ) {
+
+		// Remove our hook.
+		remove_action( 'civicrm_postSave_civicrm_setting', [ $this, 'sync_setting_override' ], 10 );
+
+		// Override.
+		$this->sync_setting_set( $value );
+
+		// Reinstate our hook.
+		add_action( 'civicrm_postSave_civicrm_setting', [ $this, 'sync_setting_override' ], 10 );
+
+	}
+
+	/**
+	 * Prevent CiviCRM from syncing Primary Email with WordPress User.
+	 *
+	 * When CiviCRM settings are saved, this method is called post-save. If the
+	 * "Sync CMS Email" setting isn't what we want it to be, override it.
+	 *
+	 * @since 0.4
+	 *
+	 * @param obj $dao The CiviCRM database access object.
+	 */
+	public function sync_setting_override( $dao ) {
+
+		// Bail if not a setting.
+		if ( ! ( $dao instanceof CRM_Core_DAO_Setting ) ) {
+			return;
+		}
+
+		// Bail if not the "Sync CMS Email" setting.
+		if ( ! isset( $dao->name ) && $dao->name != 'syncCMSEmail' ) {
+			return;
+		}
+
+		// Bail if setting is already "off".
+		if ( isset( $dao->value ) && '1' != maybe_unserialize( $dao->value ) ) {
+			return;
+		}
+
+		// Bail if our setting allows CiviCRM to handle Primary Email sync.
+		$email_sync = $this->plugin->admin->setting_get( 'user_profile_email_sync', 2 );
+		if ( $email_sync !== 1 ) {
+			return;
+		}
+
+		// Override.
+		$this->sync_setting_force( false );
+
+	}
+
+	/**
+	 * Show a notice on the CiviCRM Settings "Email Sync" metabox.
+	 *
+	 * @since 0.4
+	 */
+	public function sync_setting_notice() {
+
+		// Bail if our setting allows CiviCRM to handle Primary Email sync.
+		$email_sync = $this->plugin->admin->setting_get( 'user_profile_email_sync', 2 );
+		if ( $email_sync !== 1 ) {
+			return;
+		}
+
+		// Let people know.
+		echo '<div class="notice notice-warning inline" style="background-color: #f7f7f7;">
+			<p>' . esc_html__( 'CiviCRM Profile Sync is managing Contact Email &rarr; User Email sync.', 'civicrm-wp-profile-sync' ) . '</p>
+		</div>';
+
+	}
+
+	/**
+	 * Add some Javascript on the CiviCRM Settings "Email Sync" metabox.
+	 *
+	 * @since 0.4
+	 */
+	public function sync_setting_js() {
+
+		// Bail if our setting allows CiviCRM to handle Primary Email sync.
+		$email_sync = $this->plugin->admin->setting_get( 'user_profile_email_sync', 2 );
+		if ( $email_sync !== 1 ) {
+			return;
+		}
+
+		// Disable the dropdown.
+		echo '<script type="text/javascript">
+			jQuery("#sync_email").prop("disabled", true);
+		</script>';
+
+	}
+
+	/**
+	 * Filter the "Email Sync" submit button attributes.
+	 *
+	 * @since 0.4
+	 *
+	 * @param array $options The existing button attributes.
+	 * @return array $options The modified button attributes.
+	 */
+	public function sync_setting_button( $options ) {
+
+		// Bail if our setting allows CiviCRM to handle Primary Email sync.
+		$email_sync = $this->plugin->admin->setting_get( 'user_profile_email_sync', 2 );
+		if ( $email_sync !== 1 ) {
+			return $options;
+		}
+
+		// Disable the submit button.
+		$options['disabled'] = null;
+
+		// --<
+		return $options;
+
+	}
+
+	// -------------------------------------------------------------------------
+
+	/**
+	 * Listens for when a CiviCRM Contact's Primary Email address is about to be edited.
+	 *
+	 * @see CRM_Core_BAO_Email::add()
+	 *
+	 * @since 0.1
+	 *
+	 * @param array $args The array of CiviCRM params.
+	 */
+	public function primary_pre( $args ) {
+
+		// Get full Email record being edited.
+		$primary_email = $this->primary_record_get_by_id( $args['objectId'] );
+
+		// Sanity check.
+		if ( $primary_email === false ) {
+			return;
+		}
+
+		// Bail if it isn't the Primary Email.
+		if ( $primary_email->is_primary != 1 ) {
+			return;
+		}
+
+		// Bail if it isn't a Contact Email.
+		if ( empty( $primary_email->contact_id ) ) {
+			return;
+		}
+
+		// If our setting allows CiviCRM to handle Primary Email sync.
+		$email_sync = $this->plugin->admin->setting_get( 'user_profile_email_sync', 2 );
+		if ( $email_sync !== 1 ) {
+
+			// Remove WordPress and BuddyPress callbacks to prevent recursion.
+			$this->plugin->hooks_wp_remove();
+			$this->plugin->hooks_bp_remove();
+
+		}
+
+		/**
+		 * Fires when a CiviCRM Contact's Primary Email address is about to be
+		 * edited.
+		 *
+		 * The change of email in WordPress causes a notification email to be
+		 * sent to the WordPress User. This can be suppressed using this action
+		 * as the trigger to do so.
+		 *
+		 * @since 0.2.7
+		 *
+		 * @param integer $objectId The ID of the object.
+		 * @param object $objectRef The object.
+		 */
+		do_action( 'civicrm_wp_profile_sync_primary_email_pre_update', $args['objectId'], $args['objectRef'] );
+
+	}
+
+	/**
+	 * Listens for when a CiviCRM Contact's Primary Email address has been edited.
+	 *
+	 * @see CRM_Core_BAO_Email::add()
+	 *
+	 * @since 0.1
+	 *
+	 * @param array $args The array of CiviCRM params.
+	 */
+	public function primary_edited( $args ) {
+
+		// Bail if our setting allows CiviCRM to handle Primary Email sync.
+		$email_sync = $this->plugin->admin->setting_get( 'user_profile_email_sync', 2 );
+		if ( $email_sync !== 1 ) {
+			return;
+		}
+
+		// Get full Email record being edited.
+		$primary_email = $this->primary_record_get_by_id( $args['objectId'] );
+
+		// Sanity check.
+		if ( $primary_email === false ) {
+			return;
+		}
+
+		// Bail if it isn't the Primary Email.
+		if ( empty( $primary_email->is_primary ) || $primary_email->is_primary != 1 ) {
+			return;
+		}
+
+		// Bail if it isn't a Contact Email.
+		if ( empty( $primary_email->contact_id ) ) {
+			return;
+		}
+
+		// Get the WordPress User ID.
+		$user_id = $this->plugin->mapper->ufmatch->user_id_get_by_contact_id( $primary_email->contact_id );
+
+		// Kick out if we didn't get one.
+		if ( empty( $user_id ) ) {
+			return;
+		}
+
+		// Add User ID to args.
+		$args['user_id'] = $user_id;
+
+		// Update the WordPress User's Email.
+		$this->plugin->wp->user->email_update( $args );
+
+		/**
+		 * Broadcast that a CiviCRM Contact's Email has been synced.
+		 *
+		 * @since 0.4
+		 *
+		 * @param integer $user_id The ID of the WordPress User.
+		 * @param integer $objectId The ID of the CiviCRM Email.
+		 * @param object $objectRef The CiviCRM Email object.
+		 */
+		do_action( 'civicrm_wp_profile_sync_primary_email_synced', $user_id, $args['objectId'], $args['objectRef'] );
+
+	}
+
+	/**
+	 * Update a CiviCRM Contact's Primary Email address.
+	 *
+	 * @since 0.1
+	 * @since 0.4 Params reduced to single array.
+	 *
+	 * @param array $args The array of CiviCRM params.
+	 */
+	public function primary_update( $args ) {
+
+		// Grab User and Contact.
+		$user = $args['user'];
+		$contact = $args['ufmatch'];
+
+		// Get Primary Email record.
+		$primary_email = $this->primary_record_get_by_contact_id( $contact->contact_id );
+
+		// If there isn't a current Primary Email.
+		// phpcs:ignore Generic.CodeAnalysis.EmptyStatement.DetectedIf
+		if ( $primary_email === false ) {
+
+			// TODO: Create it? With what Location Type and Email Type?
+
+		} else {
+
+			// Has it changed?
+			if ( $primary_email->email != $user->user_email ) {
+
+				// Construct params to update the email.
+				$params = [
+					'version' => 3,
+					'id' => $primary_email->id,
+					'contact_id' => $contact->contact_id,
+					'email' => $user->user_email,
+				];
+
+				// Call the CiviCRM API.
+				$result = civicrm_api( 'Email', 'create', $params );
+
+				// Log something on failure.
+				if ( ! empty( $result['is_error'] ) && $result['is_error'] == 1 ) {
+					$e = new \Exception();
+					$trace = $e->getTraceAsString();
+					error_log( print_r( [
+						'method' => __METHOD__,
+						'message' => __( 'Could not update the email of the CiviCRM Contact.', 'civicrm-wp-profile-sync' ),
+						'result' => $result,
+						'backtrace' => $trace,
+					], true ) );
+				}
+
+			}
+
+		}
+
+	}
+
+	// -------------------------------------------------------------------------
+
+	/**
+	 * Get a CiviCRM Email record by its ID.
+	 *
+	 * @since 0.4
+	 *
+	 * @param integer $email_id The numeric ID of the CiviCRM Email record.
+	 * @return object|bool $email The CiviCRM Email record, or false on failure.
+	 */
+	public function primary_record_get_by_id( $email_id ) {
+
+		// Init return.
+		$email = false;
+
+		// Try and init CiviCRM.
+		if ( ! $this->civicrm->is_initialised() ) {
+			return $email;
+		}
+
+		// Get the current Primary Email.
+		$params = [
+			'version' => 3,
+			'id' => $email_id,
+		];
+
+		// Call the CiviCRM API.
+		$result = civicrm_api( 'Email', 'get', $params );
+
+		// Bail on failure.
+		if ( isset( $result['is_error'] ) && $result['is_error'] == '1' ) {
+			return $email;
+		}
+
+		// Bail if there are no results.
+		if ( empty( $result['values'] ) ) {
+			return $email;
+		}
+
+		// The result set should contain only one item.
+		$email = (object) array_pop( $result['values'] );
+
+		// --<
+		return $email;
+
+	}
+
+	/**
+	 * Get a CiviCRM Contact's Primary Email record.
+	 *
+	 * @since 0.4
+	 *
+	 * @param integer $contact_id The numeric ID of the CiviCRM Contact.
+	 * @return object|bool $email The CiviCRM Email record, or false on failure.
+	 */
+	public function primary_record_get_by_contact_id( $contact_id ) {
+
+		// Init return.
+		$email = false;
+
+		// Try and init CiviCRM.
+		if ( ! $this->civicrm->is_initialised() ) {
+			return $email;
+		}
+
+		// Get the current Primary Email.
+		$params = [
+			'version' => 3,
+			'contact_id' => $contact_id,
+			'is_primary' => 1,
+		];
+
+		// Call the CiviCRM API.
+		$result = civicrm_api( 'Email', 'get', $params );
+
+		// Bail on failure.
+		if ( isset( $result['is_error'] ) && $result['is_error'] == '1' ) {
+			return $email;
+		}
+
+		// Bail if there are no results.
+		if ( empty( $result['values'] ) ) {
+			return $email;
+		}
+
+		// The result set should contain only one item.
+		$email = (object) array_pop( $result['values'] );
+
+		// --<
+		return $email;
+
+	}
+
+}
