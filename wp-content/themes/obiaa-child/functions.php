e1d6abbde64f78a2a87adb64327430439a861b55
Seamus Lee <seamus.lee@jmaconsulting.biz>
2022-09-13 23:15:05 -0400

    Fix file perms

--- wp-content/themes/obiaa-child/functions.php
+++ wp-content/themes/obiaa-child/functions.php
@@ -11,3 +11,11 @@ if ( ! defined( 'ABSPATH' ) ) {
 require_once('lib/enqueue-assets.php');
 require_once('lib/theme-support.php');
 require_once('lib/customize.php');
+
+// hide update notifications
+function remove_core_updates(){
+global $wp_version;return(object) array('last_checked'=> time(),'version_checked'=> $wp_version,);
+}
+add_filter('pre_site_transient_update_core','remove_core_updates'); //hide updates for WordPress itself
+add_filter('pre_site_transient_update_plugins','remove_core_updates'); //hide updates for all plugins
+add_filter('pre_site_transient_update_themes','remove_core_updates'); //hide updates for all themes
