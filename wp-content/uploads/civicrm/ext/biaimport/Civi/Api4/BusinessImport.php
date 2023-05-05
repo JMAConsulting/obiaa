88d2003db9b066c461e9c3c8c3ccd74042681979
Seamus Lee <seamuslee001@gmail.com>
2023-05-05 16:11:40 +1000

    Update Bia Import

---
+++ wp-content/uploads/civicrm/ext/biaimport/Civi/Api4/BusinessImport.php
@@ -0,0 +1,171 @@
+<?php
+
+/*
+ +--------------------------------------------------------------------+
+ | Copyright CiviCRM LLC. All rights reserved.                        |
+ |                                                                    |
+ | This work is published under the GNU AGPLv3 license with some      |
+ | permitted exceptions and without any warranty. For full license    |
+ | and copyright information, see https://civicrm.org/licensing       |
+ +--------------------------------------------------------------------+
+ */
+
+/**
+ *
+ * @package CRM
+ * @copyright CiviCRM LLC https://civicrm.org/licensing
+ */
+
+namespace Civi\Api4;
+
+use CRM_Biaimport_ExtensionUtil as E;
+
+/**
+ * @searchable none
+ * @since 5.62
+ * @package Civi\Api4
+ */
+class BusinessImport extends Generic\AbstractEntity {
+
+
+  /**
+   * @param bool $checkPermissions
+   * @return Generic\BasicGetFieldsAction
+   */
+  public static function getFields($checkPermissions = TRUE) {
+    $spec = [
+      [
+        'name' => 'property_address',
+        'title' => E::ts('Tax Roll Address'),
+        'data_type' => 'String',
+        'required' => TRUE,
+      ],
+      [
+        'name' => 'property_tax_roll_unit',
+        'title' => E::ts('Tax Roll Unit'),
+        'data_type' => 'String',
+      ],
+      [
+        'name' => 'property_street_address',
+        'title' => E::ts('Business Mailing Address'),
+        'data_type' => 'String',
+      ],
+      [
+        'name' => 'property_street_unit',
+        'title' => E::ts('Business Mailing Unit/Suite'),
+        'data_type' => 'String',
+      ],
+      [
+        'name' => 'unit_size',
+        'title' => E::ts('Unit Size (Sq Ft)'),
+        'data_type' => 'Integer',
+      ],
+      [
+        'name' => 'unit_price',
+        'title' => E::ts('Unit Price per Sq Ft'),
+        'data_type' => 'Money',
+      ],
+      [
+        'name' => 'unit_status',
+        'title' => E::ts('Unit Status'),
+        'data_type' => 'String',
+      ],
+      [
+        'name' => 'unit_location',
+        'title' => E::ts('Unit Location (Ground Floor, Floor #)'),
+        'data_type' => 'String',
+      ],
+      [
+        'name' => 'organization_name',
+        'title' => E::ts('Business Name'),
+        'data_type' => 'String',
+      ],
+      [
+        'name' => 'first_name',
+        'title' => E::ts('Business Contact First Name'),
+        'data_type' => 'String',
+      ],
+      [
+        'name' => 'last_name',
+        'title' => E::ts('Business Contact Last Name'),
+        'data_type' => 'String',
+      ],
+      [
+        'name' => 'contact_position',
+        'title' => E::ts('Business Contact Position'),
+        'data_type' => 'String',
+      ],
+      [
+        'name' => 'phone',
+        'title' => E::ts('Telephone #'),
+        'data_type' => 'String',
+      ],
+      [
+        'name' => 'contact_email',
+        'title' => E::ts('Business Contact E-mail'),
+        'data_type' => 'String',
+      ],
+      [
+        'name' => 'website',
+        'title' => E::ts('Website URL'),
+        'data_type' => 'String',
+      ],
+      [
+        'name' => 'email',
+        'title' => E::ts('Business Email'),
+        'data_type' => 'String',
+      ],
+    ];
+    $socialMedia = [
+      'linkedin' => E::ts('LinkedIn'),
+      'facebook' => E::ts('Facebook'),
+      'instagram' => E::ts('Instagram'),
+      'twitter' => E::ts('Twitter'),
+      'ticktok' => E::ts('TickTok'),
+    ];
+    foreach ($socialMedia as $sm => $smTitle) {
+      $spec[] = [
+        'name' => $sm . '_url',
+        'title' => E::ts('%1 URL', [1 => $smTitle]),
+        'data_type' => 'String',
+      ];
+    }
+    $spec[] = [
+      'name' => 'owner_' . $owner . '_last_name',
+      'title' => E::ts('Individual Owner %1 Last Name', [1 => $owner]),
+      'data_type' => 'String',
+    ];
+    $spec[] = [
+      'name' => 'owner_' . $owner . '_mobile_phone',
+      'title' => E::ts('Individual Owner %1 Mobile Phone', [1 => $owner]),
+      'data_type' => 'String',
+    ];
+    $spec[] = [
+      'name' => 'owner_' . $owner . '_company_name',
+      'title' => E::ts('Individual Owner %1 Company Name', [1 => $owner]),
+      'data_type' => 'String',
+    ];
+    $demographicFields = [
+      'francophone' => E::ts('Francophone'),
+      'women' => E::ts('Women'),
+      'youth_39_under' => E::ts('Youth 39&Under'),
+      'lgbtiq' => E::ts('LGBTQ+'),
+      'indigenous' => E::ts('Indigenous (First Nations, Inuit or Metis)'),
+      'racialized' => E::ts('Racialized group memebr'),
+      'newcomers' => E::ts('Newcomers, immigrants and refugees'),
+      'black' => E::ts('Black community member'),
+      'disabilities' => E::ts('Persons with disabilities'),
+    ];
+    foreach ($demographicFields as $key => $label) {
+      $spec[] = [
+        'name' => $key,
+        'title' => $label,
+        'data_type' => 'Boolean',
+      ];
+    }
+    return (new Generic\BasicGetFieldsAction(__CLASS__, __FUNCTION__, function() {
+      return $spec;
+    }))->setPermissions($checkPermissions);
+  }
+
+}
---
+++ wp-content/uploads/civicrm/ext/biaimport/Civi/Api4/PropertyOwnerImport.php
@@ -0,0 +1,158 @@
+<?php
+
+/*
+ +--------------------------------------------------------------------+
+ | Copyright CiviCRM LLC. All rights reserved.                        |
+ |                                                                    |
+ | This work is published under the GNU AGPLv3 license with some      |
+ | permitted exceptions and without any warranty. For full license    |
+ | and copyright information, see https://civicrm.org/licensing       |
+ +--------------------------------------------------------------------+
+ */
+
+/**
+ *
+ * @package CRM
+ * @copyright CiviCRM LLC https://civicrm.org/licensing
+ */
+
+namespace Civi\Api4;
+
+use CRM_Biaimport_ExtensionUtil as E;
+
+/**
+ * @searchable none
+ * @since 5.62
+ * @package Civi\Api4
+ */
+class PropertyOwnerImport extends Generic\AbstractEntity {
+
+
+  /**
+   * @param bool $checkPermissions
+   * @return Generic\BasicGetFieldsAction
+   */
+  public static function getFields($checkPermissions = TRUE) {
+    $spec = [
+      [
+        'name' => 'roll_no',
+        'title' => E::ts('Assessment Roll Number'),
+        'data_type' => 'String',
+        'required' => TRUE,
+      ],
+      [
+        'name' => 'property_name',
+        'title' => E::ts('Property Name'),
+        'data_type' => 'String',
+      ],
+      [
+        'name' => 'property_address',
+        'title' => E::ts('Property Address'),
+        'data_type' => 'String',
+        'required' => TRUE,
+      ],
+      [
+        'name' => 'city',
+        'title' => E::ts('City'),
+        'data_type' => 'String',
+      ],
+      [
+        'name' => 'postal_code',
+        'title' => E::ts('postal_code'),
+        'data_type' => 'String',
+      ],
+      [
+        'name' => 'property_manager_first_name',
+        'title' => E::ts('Property Manager first name'),
+        'data_type' => 'String',
+      ],
+      [
+        'name' => 'property_manager_last_name',
+        'title' => E::ts('Property Manager last name'),
+        'data_type' => 'String',
+      ],
+      [
+        'name' => 'property_manager_email',
+        'title' => E::ts('Property Manger email'),
+        'data_type' => 'String',
+      ],
+      [
+        'name' => 'property_manager_phone',
+        'title' => E::ts('Property Manager Phone'),
+        'data_type' => 'String',
+      ],
+    ];
+    $owners = [1, 2, 3, 4];
+    foreach($owners as $owner) {
+      $spec[] = [
+        'name' => 'owner_' . $owner . '_first_name',
+        'title' => E::ts('Individual Owner %1 First Name', [1 => $owner]),
+        'data_type' => 'String',
+      ];
+      $spec[] = [
+        'name' => 'owner_' . $owner . '_last_name',
+        'title' => E::ts('Individual Owner %1 Last Name', [1 => $owner]),
+        'data_type' => 'String',
+      ];
+      $spec[] = [
+        'name' => 'owner_' . $owner . '_mobile_phone',
+        'title' => E::ts('Individual Owner %1 Mobile Phone', [1 => $owner]),
+        'data_type' => 'String',
+      ];
+      $spec[] = [
+        'name' => 'owner_' . $owner . '_company_name',
+        'title' => E::ts('Individual Owner %1 Company Name', [1 => $owner]),
+        'data_type' => 'String',
+      ];
+      $spec[] = [
+        'name' => 'owner_' . $owner . '_email',
+        'title' => E::ts('Individual Owner %1 Business Email', [1 => $owner]),
+        'data_type' => 'String',
+      ];
+      $spec[] = [
+        'name' => 'owner_' . $owner . '_phone',
+        'title' => E::ts('Individual Owner %1 Business Phone', [1 => $owner]),
+        'data_type' => 'String',
+      ];
+      $spec[] = [
+        'name' => 'owner_' . $owner . '_street_address',
+        'title' => E::ts('Individual Owner %1 Street Address', [1 => $owner]),
+        'data_type' => 'String',
+      ];
+      $spec[] = [
+        'name' => 'owner_' . $owner . '_unit',
+        'title' => E::ts('Individual Owner %1 Unit', [1 => $owner]),
+        'data_type' => 'String',
+      ];
+      $spec[] = [
+        'name' => 'owner_' . $owner . '_supplemental_address_1',
+        'title' => E::ts('Individual Owner %1 Supplemental Address', [1 => $owner]),
+        'data_type' => 'String',
+      ];
+      $spec[] = [
+        'name' => 'owner_' . $owner . '_city',
+        'title' => E::ts('Individual Owner %1 City', [1 => $owner]),
+        'data_type' => 'String',
+      ];
+      $spec[] = [
+        'name' => 'owner_' . $owner . '_province',
+        'title' => E::ts('Individual Owner %1 Province', [1 => $owner]),
+        'data_type' => 'String',
+      ];
+      $spec[] = [
+        'name' => 'owner_' . $owner . '_postal_code',
+        'title' => E::ts('Individual Owner %1 Postal Code', [1 => $owner]),
+        'data_type' => 'String',
+      ];
+      $spec[] = [
+        'name' => 'owner_' . $owner . '_country',
+        'title' => E::ts('Individual Owner %1 Country', [1 => $owner]),
+        'data_type' => 'String',
+      ];
+    }
+    return (new Generic\BasicGetFieldsAction(__CLASS__, __FUNCTION__, function() {
+      return $spec;
+    }))->setPermissions($checkPermissions);
+  }
+
+}
