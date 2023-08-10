6e3e5981ff056b5700f697e6628097e6bd7a3aaa
kashishl <116855932+kashishl@users.noreply.github.com>
2023-08-10 19:35:30 -0400

    hide update notifications for non-admins

--- wp-content/themes/obiaa-child/functions.php
+++ wp-content/themes/obiaa-child/functions.php
@@ -1,21 +1,28 @@
 <?php
 /**
-
+ *
  */
 
-if ( ! defined( 'ABSPATH' ) ) {
-	exit; // Exit if accessed directly.
+if (!defined('ABSPATH')) {
+  // Exit if accessed directly.
+  exit;
 }
 
 
-require_once('lib/enqueue-assets.php');
-require_once('lib/theme-support.php');
-require_once('lib/customize.php');
+require_once 'lib/enqueue-assets.php';
+require_once 'lib/theme-support.php';
+require_once 'lib/customize.php';
 
-// hide update notifications
-function remove_core_updates(){
-global $wp_version;return(object) array('last_checked'=> time(),'version_checked'=> $wp_version,);
+/**
+ *
+ * hide update notifications
+ *
+ */
+function hide_update_notices() {
+  $user = wp_get_current_user();
+  if (!($user->roles[0] == 'administrator')) {
+    remove_all_actions('admin_notices');
+  }
 }
-add_filter('pre_site_transient_update_core','remove_core_updates'); //hide updates for WordPress itself
-add_filter('pre_site_transient_update_plugins','remove_core_updates'); //hide updates for all plugins
-add_filter('pre_site_transient_update_themes','remove_core_updates'); //hide updates for all themes
+
+add_action('admin_head', 'hide_update_notices', 1);
