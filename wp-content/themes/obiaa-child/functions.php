a07eaaadb1c130ae4b764e6fcf97869c78100c74
kashishl <116855932+kashishl@users.noreply.github.com>
2023-08-11 16:46:28 -0400

    bug fix hide plugin notice

--- wp-content/themes/obiaa-child/functions.php
+++ wp-content/themes/obiaa-child/functions.php
@@ -14,15 +14,19 @@ require_once 'lib/theme-support.php';
 require_once 'lib/customize.php';
 
 /**
- *
- * hide update notifications
- *
+ * hide update notifications for non-admins
  */
-function hide_update_notices() {
-  $user = wp_get_current_user();
-  if (!($user->roles[0] == 'administrator')) {
-    remove_all_actions('admin_notices');
-  }
+function remove_core_updates() {	
+	global $wp_version;
+	return (object) array('last_checked' => time(), 'version_checked' => $wp_version);
 }
 
-add_action('admin_head', 'hide_update_notices', 1);
+$user = wp_get_current_user();
+if (!empty($user->roles) && $user->roles[0] != 'administrator') {
+	//hide updates for WordPress itself
+	add_filter('pre_site_transient_update_core', 'remove_core_updates');
+	//hide updates for all plugins
+	add_filter('pre_site_transient_update_plugins', 'remove_core_updates');
+	//hide updates for all themes
+	add_filter('pre_site_transient_update_themes', 'remove_core_updates');
+}
