1baa38871bd86a9597e4d958c44528761c4746fb
kashishl <116855932+kashishl@users.noreply.github.com>
2023-08-11 16:50:33 -0400

    fixed formatting

--- wp-content/themes/obiaa-child/functions.php
+++ wp-content/themes/obiaa-child/functions.php
@@ -8,7 +8,6 @@ if (!defined('ABSPATH')) {
   exit;
 }
 
-
 require_once 'lib/enqueue-assets.php';
 require_once 'lib/theme-support.php';
 require_once 'lib/customize.php';
@@ -16,17 +15,17 @@ require_once 'lib/customize.php';
 /**
  * hide update notifications for non-admins
  */
-function remove_core_updates() {	
-	global $wp_version;
-	return (object) array('last_checked' => time(), 'version_checked' => $wp_version);
+function remove_core_updates() {
+  global $wp_version;
+  return (object) array('last_checked' => time(), 'version_checked' => $wp_version);
 }
 
 $user = wp_get_current_user();
 if (!empty($user->roles) && $user->roles[0] != 'administrator') {
-	//hide updates for WordPress itself
-	add_filter('pre_site_transient_update_core', 'remove_core_updates');
-	//hide updates for all plugins
-	add_filter('pre_site_transient_update_plugins', 'remove_core_updates');
-	//hide updates for all themes
-	add_filter('pre_site_transient_update_themes', 'remove_core_updates');
+  //hide updates for WordPress itself
+  add_filter('pre_site_transient_update_core', 'remove_core_updates');
+  //hide updates for all plugins
+  add_filter('pre_site_transient_update_plugins', 'remove_core_updates');
+  //hide updates for all themes
+  add_filter('pre_site_transient_update_themes', 'remove_core_updates');
 }
