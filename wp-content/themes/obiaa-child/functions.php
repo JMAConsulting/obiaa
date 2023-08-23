e78a0259414c6c06c1fe5727cf6928b3f561c797
Monish Deb <monish.deb@jmaconsulting.biz>
2023-08-23 03:30:47 -0400

    Wordpress 6.3 upgrade

--- wp-content/themes/obiaa-child/functions.php
+++ wp-content/themes/obiaa-child/functions.php
@@ -16,6 +16,6 @@ require_once('lib/customize.php');
 function remove_core_updates(){
 global $wp_version;return(object) array('last_checked'=> time(),'version_checked'=> $wp_version,);
 }
-add_filter('pre_site_transient_update_core','remove_core_updates'); //hide updates for WordPress itself
-add_filter('pre_site_transient_update_plugins','remove_core_updates'); //hide updates for all plugins
-add_filter('pre_site_transient_update_themes','remove_core_updates'); //hide updates for all themes
+//add_filter('pre_site_transient_update_core','remove_core_updates'); //hide updates for WordPress itself
+//add_filter('pre_site_transient_update_plugins','remove_core_updates'); //hide updates for all plugins
+//add_filter('pre_site_transient_update_themes','remove_core_updates'); //hide updates for all themes
