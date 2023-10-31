b276abf0f01c591d8f9f138190e25f2046283146
Lauren Smillie <laurensmillie@OC-JMA.local>
2023-10-31 12:38:58 -0400

    Making CivicrmPropertyLog entity
    (cherry picked from commit 80126a7ccbc049d5696baa2046f7ac919794219c)

---
+++ wp-content/uploads/civicrm/ext/biasync/Civi/Api4/CivicrmPropertyLog.php
@@ -0,0 +1,13 @@
+<?php
+namespace Civi\Api4;
+
+/**
+ * CivicrmPropertyLog entity.
+ *
+ * Provided by the OBIAA Central Sync extension.
+ *
+ * @package Civi\Api4
+ */
+class CivicrmPropertyLog extends Generic\DAOEntity {
+
+}
