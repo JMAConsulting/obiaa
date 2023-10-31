f130e64f84687023a26ad406cf5801c2f3d37967
pipeline <pipeline@jmaconsulting.biz>
2023-10-30 20:14:29 -0600

    Added biasynchandler extension

---
+++ wp-content/uploads/civicrm/ext/biasynchandler/Civi/Api4/Biasync.php
@@ -0,0 +1,13 @@
+<?php
+namespace Civi\Api4;
+
+/**
+ * Biasync entity.
+ *
+ * Provided by the biasynchandler extension.
+ *
+ * @package Civi\Api4
+ */
+class Biasync extends Generic\DAOEntity {
+
+}
