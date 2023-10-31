25bc78c5c16dd88cd2c0a9d2a0b2a3f315c9b61e
Lauren Smillie <laurensmillie@OC-JMA.local>
2023-10-31 13:17:04 -0400

    Updating log entity and modifying call for only unsynced properties

--- wp-content/uploads/civicrm/ext/biasync/CRM/Biasync/BAO/CivicrmPropertyLog.php
+++ wp-content/uploads/civicrm/ext/biasync/CRM/Biasync/BAO/CivicrmPropertyLog.php
@@ -5,14 +5,4 @@ use CRM_Biasync_ExtensionUtil as E;
 
 class CRM_Biasync_BAO_CivicrmPropertyLog extends CRM_Biasync_DAO_CivicrmPropertyLog {
 
-    public static function retrieve(array $params) {
-        $options = [];
-        $properties = \Civi\Api4\CivicrmPropertyLog::get(FALSE)
-         ->setLimit(100)
-         ->execute();
-       foreach ($properties as $property) {
-         $options[$property['id']] = $property;
-       }
-       return $options;
-   }
 }
