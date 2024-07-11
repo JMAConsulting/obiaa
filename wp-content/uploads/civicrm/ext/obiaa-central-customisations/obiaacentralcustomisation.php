8381fa9faf9a97d7846e17d4ad03e3215eedd544
Seamus Lee <seamuslee001@gmail.com>
2024-07-12 07:47:45 +1000

    Run Civix upgrade on obiaa central customisations

--- wp-content/uploads/civicrm/ext/obiaa-central-customisations/obiaacentralcustomisation.php
+++ wp-content/uploads/civicrm/ext/obiaa-central-customisations/obiaacentralcustomisation.php
@@ -25,59 +25,12 @@ function obiaacentralcustomisation_civicrm_install() {
 }
 
 /**
- * Implements hook_civicrm_postInstall().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_postInstall
- */
-function obiaacentralcustomisation_civicrm_postInstall() {
-  _obiaacentralcustomisation_civix_civicrm_postInstall();
-}
-
-/**
- * Implements hook_civicrm_uninstall().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_uninstall
- */
-function obiaacentralcustomisation_civicrm_uninstall() {
-  _obiaacentralcustomisation_civix_civicrm_uninstall();
-}
-
-/**
  * Implements hook_civicrm_enable().
  *
  * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_enable
  */
 function obiaacentralcustomisation_civicrm_enable() {
   _obiaacentralcustomisation_civix_civicrm_enable();
-}
-
-/**
- * Implements hook_civicrm_disable().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_disable
- */
-function obiaacentralcustomisation_civicrm_disable() {
-  _obiaacentralcustomisation_civix_civicrm_disable();
-}
-
-/**
- * Implements hook_civicrm_upgrade().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_upgrade
- */
-function obiaacentralcustomisation_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
-  return _obiaacentralcustomisation_civix_civicrm_upgrade($op, $queue);
-}
-
-/**
- * Implements hook_civicrm_entityTypes().
- *
- * Declare entity types provided by this module.
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_entityTypes
- */
-function obiaacentralcustomisation_civicrm_entityTypes(&$entityTypes) {
-  _obiaacentralcustomisation_civix_civicrm_entityTypes($entityTypes);
 }
 
 // --- Functions below this ship commented out. Uncomment as required. ---
