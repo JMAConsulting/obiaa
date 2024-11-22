e0442c027a6be0f06e0e0c049d1fe0f382ef08ad
Monish Deb <monish.deb@jmaconsulting.biz>
2024-11-22 09:06:06 -0500

    Add missing plugins

---
+++ wp-content/plugins/wp-persistent-login/vendor/composer/installed.php
@@ -0,0 +1,41 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => 'bbf5e9af5b90541a1dc3c3ef47571b11c7b78ecb',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'bbf5e9af5b90541a1dc3c3ef47571b11c7b78ecb',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'psr/cache' => array(
+            'pretty_version' => '1.0.1',
+            'version' => '1.0.1.0',
+            'reference' => 'd11b50ad223250cf17b86e38383413f5a6764bf8',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../psr/cache',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'whichbrowser/parser' => array(
+            'pretty_version' => 'v2.1.7',
+            'version' => '2.1.7.0',
+            'reference' => '1044880bc792dbce5948fbff22ae731c43c280d9',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../whichbrowser/parser',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
