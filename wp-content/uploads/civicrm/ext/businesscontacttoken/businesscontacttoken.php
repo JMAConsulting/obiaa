9c7acd659a03152d27d6713263ad5ae02fbc6db2
Rafe Murray <rafe.murray@jmaconsulting.biz>
2025-08-20 10:46:44 -0400

    Add checksum to tokens

--- wp-content/uploads/civicrm/ext/businesscontacttoken/businesscontacttoken.php
+++ wp-content/uploads/civicrm/ext/businesscontacttoken/businesscontacttoken.php
@@ -10,8 +10,7 @@ use CRM_Businesscontacttoken_ExtensionUtil as E;
  *
  * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_config/
  */
-function businesscontacttoken_civicrm_config(&$config): void
-{
+function businesscontacttoken_civicrm_config(&$config): void {
   _businesscontacttoken_civix_civicrm_config($config);
 }
 
@@ -20,8 +19,7 @@ function businesscontacttoken_civicrm_config(&$config): void
  *
  * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_install
  */
-function businesscontacttoken_civicrm_install(): void
-{
+function businesscontacttoken_civicrm_install(): void {
   _businesscontacttoken_civix_civicrm_install();
 }
 
@@ -30,29 +28,25 @@ function businesscontacttoken_civicrm_install(): void
  *
  * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_enable
  */
-function businesscontacttoken_civicrm_enable(): void
-{
+function businesscontacttoken_civicrm_enable(): void {
   _businesscontacttoken_civix_civicrm_enable();
 }
 
-function businesscontacttoken_civicrm_container($container)
-{
+function businesscontacttoken_civicrm_container($container) {
   $container->addResource(new \Symfony\Component\Config\Resource\FileResource(__FILE__));
   $container->findDefinition('dispatcher')->addMethodCall('addListener', ['civi.token.eval', 'businesscontacttoken_evaluate_token']);
   $container->findDefinition('dispatcher')->addMethodCall('addListener', ['civi.token.list', 'businesscontacttoken_register_token']);
 }
 
-function businesscontacttoken_register_token(\Civi\Token\Event\TokenRegisterEvent $e)
-{
+function businesscontacttoken_register_token(\Civi\Token\Event\TokenRegisterEvent $e) {
   $context = $e->getTokenProcessor()->context;
   if (key_exists('schema', $context) && in_array('contactId', $context['schema'])) {
     $e->entity(CRM_Businesscontacttoken_Token::TOKEN)
-      ->register('contact_id', E::ts('Add Business Links'));
+      ->register('contact_id', E::ts('Update Business Links'));
   }
 }
 
-function businesscontacttoken_evaluate_token(\Civi\Token\Event\TokenValueEvent $e)
-{
+function businesscontacttoken_evaluate_token(\Civi\Token\Event\TokenValueEvent $e) {
   $context = $e->getTokenProcessor()->context;
   foreach ($e->getRows() as $row) {
     if (isset($row->context['contactId'])) {
