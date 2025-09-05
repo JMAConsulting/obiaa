edd2af57c51bfecd1872e590952f010ba8d047a2
Edsel Lopez <edsel.lopez@jmaconsulting.biz>
2025-09-05 07:50:38 -0400

    Fixes for error

--- wp-content/plugins/acf-add-business/field-defaults.php
+++ wp-content/plugins/acf-add-business/field-defaults.php
@@ -152,7 +152,7 @@ function getContactDetails(): array|null {
   return $cid ? \Civi\Api4\Contact::get(FALSE)
     ->addSelect('*', 'email_primary.email', 'phone_primary.phone')
     ->addWhere('id', '=', $cid)
-    ->single() : null;
+    ->execute()->first() : null;
 }
 
 /**
@@ -165,5 +165,5 @@ function getBusinessDetails(): array|null {
     ->addWhere('contact_sub_type', '=', 'Members_Businesses_')
     ->addJoin('Website AS website', 'LEFT', ['id', '=', 'website.contact_id'])
     ->addSelect('*', 'custom.*', 'email_primary.email', 'phone_primary.phone', 'website.url')
-    ->single() : null;
+    ->execute()->first() : null;
 }
