234a201994e6d519ef559c3bd54257b012880b94
Seamus Lee <seamuslee001@gmail.com>
2025-11-01 09:56:31 +1100

    Update WP Persistent login to 3.0.2

--- wp-content/plugins/wp-persistent-login/wp_persistent_login.php
+++ wp-content/plugins/wp-persistent-login/wp_persistent_login.php
@@ -2,14 +2,14 @@
 
 /**
  *
- *   Plugin Name: WordPress Persistent Login
+ *   Plugin Name: Persistent Login
  *   Plugin URI: https://persistentlogin.com/
  *   Description: Keep users logged into your website securely, and allows you to limit the number of active logins.
  *   Author: Luke Seager
  *   Author URI:  https://persistentlogin.com/
  * 	 Text Domain: wp-persistent-login
  *   Domain Path: /languages
- *   Version: 2.1.4
+ *   Version: 3.0.2
  *
  *
  */
@@ -36,17 +36,20 @@ if ( function_exists( 'persistent_login' ) ) {
     persistent_login()->set_basename( false, __FILE__ );
 } else {
     // definitions to use throughout application.
-    define( 'WPPL_DATABASE_VERSION', '2.0.10' );
+    define( 'WPPL_DATABASE_VERSION', '3.0.1' );
     define( 'WPPL_DATABASE_NAME', 'persistent_logins' );
     define( 'WPPL_PLUGIN_PATH', plugin_dir_path( __FILE__ ) );
     define( 'WPPL_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
     define( 'WPPL_PLUGIN_BASENAME', plugin_basename( __FILE__ ) );
     define( 'WPPL_SETTINGS_AREA', get_admin_url() . 'users.php' );
     define( 'WPPL_SETTINGS_PAGE', WPPL_SETTINGS_AREA . '?page=wp-persistent-login' );
-    define( 'WPPL_ACCOUNT_PAGE', WPPL_SETTINGS_AREA . '?page=wp-persistent-login-account' );
+    define( 'WPPL_ACCOUNT_PAGE', get_admin_url() . 'admin.php?page=wp-persistent-login-account' );
     define( 'WPPL_UPGRADE_PAGE', WPPL_SETTINGS_AREA . '?billing_cycle=annual&page=wp-persistent-login-pricing' );
-    define( 'WPPL_SUPPORT_PAGE', WPPL_SETTINGS_AREA . '?page=wp-persistent-login-contact' );
-    define( 'WPPL_TEXT_DOMAIN', 'wp-persitent-login' );
+    define( 'WPPL_TRIAL_UPGRADE_PAGE', WPPL_SETTINGS_AREA . '?billing_cycle=annual&trial=true&page=wp-persistent-login-pricing' );
+    define( 'WPPL_REVIEW_PAGE', 'https://wordpress.org/support/plugin/wp-persistent-login/reviews/#new-post' );
+    define( 'WPPL_TEXT_DOMAIN', 'wp-persistent-login' );
+    // load composor packages.
+    require_once WPPL_PLUGIN_PATH . '/vendor/autoload.php';
     // Load text domain
     add_action( 'init', 'wp_persistent_login_load_textdomain' );
     function wp_persistent_login_load_textdomain() {
@@ -54,20 +57,21 @@ if ( function_exists( 'persistent_login' ) ) {
     }
 
     // Load freemius.
-    require WPPL_PLUGIN_PATH . '/includes/freemius.php';
+    require_once WPPL_PLUGIN_PATH . '/includes/freemius.php';
     // Load installation file.
-    require WPPL_PLUGIN_PATH . '/includes/install.php';
+    require_once WPPL_PLUGIN_PATH . '/includes/install.php';
     register_activation_hook( __FILE__, 'persistent_login_activate' );
     // Load database upgrade file.
-    require WPPL_PLUGIN_PATH . '/includes/database-upgrades.php';
+    require_once WPPL_PLUGIN_PATH . '/includes/database-upgrades.php';
     // Load uninstall cleanup file.
-    require WPPL_PLUGIN_PATH . '/includes/uninstall.php';
+    require_once WPPL_PLUGIN_PATH . '/includes/uninstall.php';
     persistent_login()->add_action( 'after_uninstall', 'persistent_login_uninstall_cleanup' );
-    // load composor packages.
-    require WPPL_PLUGIN_PATH . '/vendor/autoload.php';
-    // autoload wp persistent login classes.
-    require WPPL_PLUGIN_PATH . '/classes/autoload.php';
-    define( 'WPPL_PR', false );
+    // autoload persistent login classes.
+    require_once WPPL_PLUGIN_PATH . '/classes/autoload.php';
+    // secondary definitions used throughout application.
+    $wppl_pr_enabled = false;
+    define( 'WPPL_PR', $wppl_pr_enabled );
+    define( 'WPPL_SUPPORT_PAGE', 'https://wordpress.org/support/plugin/wp-persistent-login/' );
     new WP_Persistent_Login();
     new WP_Persistent_Login_Admin();
     new WP_Persistent_Login_Profile();
@@ -76,7 +80,7 @@ if ( function_exists( 'persistent_login' ) ) {
     new WP_Persistent_Login_Email();
     new WP_Persistent_Login_User_Count();
     /**
-     * Action hook to execute after WP Persistent Login plugin init.
+     * Action hook to execute after Persistent Login plugin init.
      *
      * Use this hook to init addons.
      *
@@ -93,7 +97,7 @@ if ( function_exists( 'persistent_login' ) ) {
         function wppl_on_mail_error_log(  $wp_error  ) {
             // turn wp_error into a string.
             $wp_error = $wp_error->get_error_message();
-            error_log( 'WP Persistent Login: ' . $wp_error );
+            error_log( 'Persistent Login: ' . $wp_error );
         }
 
     }
