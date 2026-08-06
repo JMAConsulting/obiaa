94242e5cc89f2515f018680881465db4e6db9319
Monish Deb <monish.deb@jmaconsulting.biz>
2026-08-06 09:44:39 -0400

    CiviCRM 6.17.1 sec upgrade

--- wp-content/plugins/civicrm/civicrm/civicrm-version.php
+++ wp-content/plugins/civicrm/civicrm/civicrm-version.php
@@ -1,7 +1,7 @@
 <?php
 /** @deprecated */
 function civicrmVersion( ) {
-  return array( 'version'  => '6.15.5',
+  return array( 'version'  => '6.17.1',
                 'cms'      => 'Wordpress',
                 'revision' => '' );
 }
