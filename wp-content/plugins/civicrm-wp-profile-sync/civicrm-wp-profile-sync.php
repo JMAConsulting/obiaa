477716065edfa992b47d468a06875c3cbc85f6aa
Edsel Lopez <edsel.lopez@jmaconsulting.biz>
2026-09-25 03:13:09 -0400

    Update plugins: Advanced Custom Fields Pro 6.8.10, CiviCRM Admin Utilities 1.1.2, CiviCRM WP Profile Sync 0.7.5 (reinstalled after failed update), Groups 4.8.0, Wordfence 9.0.1

--- wp-content/plugins/civicrm-wp-profile-sync/civicrm-wp-profile-sync.php
+++ wp-content/plugins/civicrm-wp-profile-sync/civicrm-wp-profile-sync.php
@@ -6,7 +6,7 @@
  * Description:       Keeps a WordPress User profile in sync with a CiviCRM Contact and integrates WordPress and CiviCRM Entities with data synced via Advanced Custom Fields.
  * Plugin URI:        https://github.com/christianwach/civicrm-wp-profile-sync
  * GitHub Plugin URI: https://github.com/christianwach/civicrm-wp-profile-sync
- * Version:           0.7.3
+ * Version:           0.7.5
  * Author:            Christian Wach
  * Author URI:        https://haystack.co.uk
  * License:           GPLv2 or later
@@ -35,7 +35,7 @@
 defined( 'ABSPATH' ) || exit;
 
 // Set plugin version here.
-define( 'CIVICRM_WP_PROFILE_SYNC_VERSION', '0.7.3' );
+define( 'CIVICRM_WP_PROFILE_SYNC_VERSION', '0.7.5' );
 
 // Set our bulk operations flag here.
 if ( ! defined( 'CIVICRM_WP_PROFILE_SYNC_BULK' ) ) {
@@ -135,7 +135,7 @@ class CiviCRM_WP_Profile_Sync {
 	public $cai;
 
 	/**
-	 * Initialises this object.
+	 * Constructor.
 	 *
 	 * @since 0.1
 	 */
@@ -151,7 +151,7 @@ class CiviCRM_WP_Profile_Sync {
 	 *
 	 * @since 0.5
 	 */
-	public function initialise() {
+	private function initialise() {
 
 		// Only do this once.
 		static $done;
@@ -159,10 +159,13 @@ class CiviCRM_WP_Profile_Sync {
 			return;
 		}
 
-		// Include files.
-		$this->include_files();
+		// Maybe include WP-CLI command.
+		if ( defined( 'WP_CLI' ) && WP_CLI ) {
+			require CIVICRM_WP_PROFILE_SYNC_PATH . 'includes/wp-cli/wp-cli-profilesync.php';
+		}
 
-		// Set up objects and references.
+		// Bootstrap plugin.
+		$this->include_files();
 		$this->setup_objects();
 
 		/**
@@ -182,7 +185,7 @@ class CiviCRM_WP_Profile_Sync {
 	 *
 	 * @since 0.4
 	 */
-	public function include_files() {
+	private function include_files() {
 
 		// Load our class files.
 		require CIVICRM_WP_PROFILE_SYNC_PATH . 'includes/admin/cwps-admin.php';
@@ -200,7 +203,7 @@ class CiviCRM_WP_Profile_Sync {
 	 *
 	 * @since 0.4
 	 */
-	public function setup_objects() {
+	private function setup_objects() {
 
 		// Initialise objects.
 		$this->admin   = new CiviCRM_WP_Profile_Sync_Admin( $this );
